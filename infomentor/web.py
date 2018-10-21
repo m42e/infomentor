86ad36a2633e18da19d79164a9b59d4dc04e1a82
Matthias Bilger <matthias@bilger.info>
2018-10-21 21:43:11 +0200

    added minimal webinterface

--- infomentor/web.py
+++ infomentor/web.py
@@ -1,5 +1,5 @@
-from infomentor import models, db
-from flask import Flask, render_template
+from infomentor import model, db
+from flask import Flask, render_template, redirect, url_for, request
 from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
@@ -9,9 +9,29 @@ Bootstrap(app)
 def home():
 	return render_template('notfound.html')
 
-@app.route('/addlogin/')
+@app.route('/addlogin')
 def extra():
 	return render_template('addlogin.html')
+
+@app.route('/create', methods=['POST'])
+def create():
+    if request.form['accesscode'] != 'fhKjzgV/BXWq4YRxUPO4qYlHWCDf':
+        return redirect(url_for('home'))
+    session = db.get_db()
+    username = request.form['username']
+    existing_user = session.query(model.User).filter(model.User.name == username).one_or_none()
+    if existing_user is not None:
+        return redirect(url_for('home'))
+
+    password = request.form['password']
+    user = model.User(name=username, password=password)
+    if request.form['notify'] == 'mail':
+        user.notification = [model.Notification(ntype=model.Notification.Types.EMAIL, info=request.form['info'])]
+    else:
+        user.notification = [model.Notification(ntype=model.Notification.Types.PUSHOVER, info=request.form['info'])]
+    session.add(user)
+    session.commit()
+    return "success"
 
 if __name__ == '__main__':
 	app.run(debug=True)
